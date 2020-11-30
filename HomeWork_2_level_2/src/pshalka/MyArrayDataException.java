65133e5c319de8b3b2a2a38a728d119ea21bcd14
igor <garik200268@gmail.com>
2020-11-30 21:13:58 +0900

    Work 2

---
+++ HomeWork_2_level_2/src/pshalka/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package pshalka;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ HomeWork_2_level_2/src/pshalka/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package pshalka;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+
+    }
+}
