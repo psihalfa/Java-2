2c0422c6632c68e7fca7f306c1becf28a4a6b84a
igor <garik200268@gmail.com>
2020-11-30 13:29:10 +0900

    Work1

---
+++ HomeWork_1_level_2/src/pashalka/Treadmill.java
@@ -0,0 +1,15 @@
+package pashalka;
+
+public class Treadmill {
+    private int range;
+
+    public Treadmill(int range) {
+        this.range = range;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) { this.range = range; }
+}
---
+++ HomeWork_1_level_2/src/pashalka/Wall.java
@@ -0,0 +1,13 @@
+package pashalka;
+
+public class Wall {
+    private int Height;
+
+    public Wall(int height) {
+        this.Height = height;
+    }
+
+    public int getHeight() { return Height; }
+
+    public void setHeight(int height) { Height = height; }
+}
